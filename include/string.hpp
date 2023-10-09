e450fe93ac05fef00b61cdbb78baa8aab7c5bb66
ycjinyi <liujinyicn@foxmail.com>
2023-10-09 16:23:46 +0800

    增加string类的实现

---
+++ include/string.hpp
@@ -0,0 +1,79 @@
+#ifndef STRING_H
+#define STRING_H
+#include <memory>
+#include <iostream>
+#include <string.h>
+class string {
+friend std::ostream& operator<< (std::ostream& os, const string& t);
+friend string operator+ (const string& lhs, const string& rhs);
+public:
+    //构造和析构函数
+    string(const char* ptr = nullptr) {
+        //std::cout << "string(const char* ptr = nullptr)" << std::endl;
+        if(ptr != nullptr) {
+            ptr_.reset(new char[strlen(ptr) + 1]);
+            strcpy(ptr_.get(), ptr);
+            return;
+        }
+        ptr_.reset(new char[1] {'\0'});
+    }
+    string(const string& rhs) {
+        //std::cout << "string(const string& rhs)" << std::endl;
+        ptr_.reset(new char[strlen(rhs.ptr_.get()) + 1]);
+        strcpy(ptr_.get(), rhs.ptr_.get());
+    }
+    string(string&& rhs) noexcept {
+        //std::cout << "string(string&& rhs) noexcept" << std::endl;
+        ptr_ = std::move(rhs.ptr_);
+    }
+    ~string() = default;
+
+    //操作符
+    string& operator= (const string& rhs) {
+        //std::cout << "string& operator= (const string& rhs)" << std::endl;
+        if(this == &rhs) return *this;
+        ptr_.reset(new char[strlen(rhs.ptr_.get()) + 1]);
+        strcpy(ptr_.get(), rhs.ptr_.get());
+        return *this;
+    }
+    string& operator= (string&& rhs) noexcept {
+        //std::cout << "string& operator= (string&& rhs)" << std::endl;
+        ptr_ = std::move(rhs.ptr_);
+        return *this;
+    }
+    //注意引用的返回，一定不是局部变量
+    char& operator[](size_t p) {
+        return ptr_[p];
+    }
+    const char& operator[](size_t p) const {
+        return ptr_[p];
+    }
+    bool operator> (const string& rhs) const{
+        return strcmp(ptr_.get(), rhs.ptr_.get()) > 0;
+    }
+    bool operator< (const string& rhs) const {
+        return strcmp(ptr_.get(), rhs.ptr_.get()) < 0;
+    }
+    bool operator== (const string& rhs) const {
+        return strcmp(ptr_.get(), rhs.ptr_.get()) == 0;
+    }
+
+    //其它操作
+    const char* c_str() const {
+        return ptr_.get();
+    }
+private:
+    std::unique_ptr<char []> ptr_;
+};
+std::ostream& operator<< (std::ostream& os, const string& t) {
+    os << t.ptr_.get();
+    return os;
+} 
+string operator+ (const string& lhs, const string& rhs) {
+    string temp(new char[strlen(lhs.ptr_.get()) + strlen(rhs.ptr_.get()) + 1]);
+    strcpy(temp.ptr_.get(), lhs.ptr_.get());
+    strcat(temp.ptr_.get(), rhs.ptr_.get());
+    //由于返回值优化，这里并没有再构造新的对象
+    return temp;
+}
+#endif
--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "vector.hpp"
+#include "string.hpp"
 class Test {
 public:
     Test() { std::cout << "Test()" << std::endl;}
@@ -8,7 +9,12 @@ public:
     Test(Test&&) { std::cout << "Test(Test&&)" << std::endl;} 
     ~Test() { std::cout << "~Test()" << std::endl; }
 };
+std::ostream& operator<< (std::ostream& os, const Test& t) {
+    os << "cout << Test";
+    return os;
+}
 int main() {
+    /*
     vector<Test> vec;
     vec.reserve(20);
     Test a(10);
@@ -21,5 +27,17 @@ int main() {
     std::cout << "------" << std::endl;
     vec.push_back(Test());
     vec.emplace_back(Test());
+    std::cout << vec[2] << std::endl;
+    */
+
+    string s1 = "aaa";
+    std::cout << s1 << std::endl;
+    string s2 = std::move(s1);
+    std::cout << s2 << std::endl;
+    string s3 = "ddd" + s2;
+    std::cout << s3 << std::endl;
+    bool eq = s3 == s2;
+    std::cout << eq << std::endl;
+
     return 0;
 }
